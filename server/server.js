4269004a643b0804c16c6d490e2ea5aec266fa79
Vigneshwaran <125852792+vigneshwaranbalamurugan@users.noreply.github.com>
2024-03-29 13:08:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import path from 'path';
+import core from 'cors';
 import RegisterRouter from './routes/Register.js';
 
 dotenv.config();
@@ -16,7 +17,7 @@ mongoose
     });
 
 const app = express();
-app.use(cors());
+app.use(core());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,6 +26,13 @@ app.use((err, req, res, next) => {
 });
 
 app.use('/auth',RegisterRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 const port = process.env.PORT;
 app.listen(port, () => {
